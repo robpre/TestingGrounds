ccbcdf74a8474ddf8e9ebc1649a75c6edaf4925f
Robert Preus-MacLaren <rpprroger@gmail.com>
2019-04-27 11:18:11 +0100

    Using C++ for the choose next waypoint

---
+++ Source/TestingGrounds/Public/ChooseNextWaypoint.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "ChooseNextWaypoint.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+	// Methods are private by default
+	/** starts this task, should return Succeeded, Failed or InProgress
+	 *  (use FinishLatentTask() when returning InProgress)
+	 * this function should be considered as const (don't modify state of object) if node is not instanced!
+	 *
+	 */
+	EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
