98971dd0666a7998ab824fb4f3c98119170346da
Robert Preus-MacLaren <rpprroger@gmail.com>
2019-04-27 19:41:03 +0100

    Move HUD

--- Source/TestingGrounds/TestingGroundsGameMode.cpp
+++ Source/TestingGrounds/TestingGroundsGameMode.cpp
@@ -1,7 +1,7 @@
 // Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
 #include "TestingGroundsGameMode.h"
-#include "TestingGroundsHUD.h"
+#include "FirstPersonHUD.h"
 #include "Player/FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
@@ -13,5 +13,5 @@ ATestingGroundsGameMode::ATestingGroundsGameMode()
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
-	HUDClass = ATestingGroundsHUD::StaticClass();
+	HUDClass = AFirstPersonHUD::StaticClass();
 }
